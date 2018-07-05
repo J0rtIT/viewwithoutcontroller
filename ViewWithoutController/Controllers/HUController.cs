e626a44143f67950b7e028a0e1ad0c7772a6a0f1
Jose Ortega <jortega928@yahoo.com>
2018-07-04 19:15:43 -0500

    Add project files.

---
+++ ViewWithoutController/Controllers/HUController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ViewWithoutController.Controllers
+{
+    public class HUController : Controller
+    {
+        // GET: HU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            try
+            {
+                View(actionName).ExecuteResult(ControllerContext);
+            }
+            catch (Exception ex)
+            {
+                Response.Redirect("PageNotFound");
+            }
+            //            base.HandleUnknownAction(actionName);
+        }
+
+    }
+}
